dcd8674100948c34191afd4cadd0367cd2c793c8
Matt Krueger <mkrueger@rstms.net>
2021-09-11 17:42:08 -0500

    initial commit - forked from: audreyfeldroy/cookiecutter/pypackage

---
+++ setup.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-python-pypi-click',
+    packages=[],
+    version='0.1.0',
+    description='cookiecutter template for pypi cli project',
+    author='Matt Krueger',
+    license='MIT',
+    author_email='mkrueger@rstms.net",
+    url='https://github.com/rstms/cookiecutter-python-pypi-click',
+    keywords=['cookiecutter', 'template', 'package', ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
---
+++ {{cookiecutter.project_slug}}/make.include/browser.mk
@@ -0,0 +1,14 @@
+# browser: run the system's browser if available
+
+define BROWSER_PYSCRIPT
+import os, webbrowser, sys
+
+from urllib.request import pathname2url
+
+webbrowser.open("file://" + pathname2url(os.path.abspath(sys.argv[1])))
+endef
+export BROWSER_PYSCRIPT
+
+BROWSER := python -c "$$BROWSER_PYSCRIPT"
+
+define browser = $(shell python -c "$(BROWSER_PYSCRIPT)" $(1))
