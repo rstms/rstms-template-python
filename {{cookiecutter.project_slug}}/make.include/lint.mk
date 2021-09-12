dd4eeee3b6a199b2d087c7e7ffc3ed960d89db78
Matt Krueger <mkrueger@rstms.net>
2021-09-11 23:23:25 -0500

    fix typo in template escape

--- {{cookiecutter.project_slug}}/make.include/lint.mk
+++ {{cookiecutter.project_slug}}/make.include/lint.mk
@@ -5,7 +5,7 @@ fmt:  ## blacken python source
 	black $(project) tests
 {%- endif %}
 
-lint:%{ if cookiecutter.use_black == 'y' %} fmt{%- endif %}  ## check style with flake8
+lint:{% if cookiecutter.use_black == 'y' %} fmt{%- endif %}  ## check style with flake8
 	flake8 $(project) tests
 {% if cookiecutter.use_black == 'y' %}
 	black --check $(project) tests
