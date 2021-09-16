ab833fb0f328f1291b1ae231c4cd489e65087a82
Matt Krueger <mkrueger@rstms.net>
2021-09-16 15:19:49 -0500

    update version makefile

--- {{cookiecutter.project_slug}}/make.include/version.mk
+++ {{cookiecutter.project_slug}}/make.include/version.mk
@@ -3,26 +3,22 @@
 # - Prevent version changes with uncommited changes
 # - tag and commit version changes
 # - Configure files to be edited in `setup.cfg`
+# - Use 'lightweight tags'
 
-# bump patch level
-bump-patch:
-	bumpversion patch
+bump-patch: ## bump patch level
+	bumpversion patch --tag-message ''
 
-# bump minor version, reset patch to zero
-bump-minor:
-	bumpversion minor 
+bump-minor: ## bump minor version, reset patch to zero
+	bumpversion minor --tag-message ''
 
-# bump version, reset minor and patch to zero
-bump-major:
-	bumpversion major 
+bump-major: ## bump version, reset minor and patch to zero
+	bumpversion major --tag-message ''
 
-# update timestamp if sources have changed
-timestamp: .timestamp
-
+timestamp: .timestamp ## update timestamp if sources have changed
 .timestamp: $(src)
-	@sed -i  -E repeat/version.py -e "s/(.*__timestamp__.*=).*/\\1 '$(date -Isec)'/"
+	sed -E -i $(project)/__init__.py -e "s/(.*__timestamp__.*=).*/\1 \"$$(date -Isec)\"/"
 	@touch $@
 	@echo "Timestamp Updated."
 
-version-clean:
+version-clean: # clean up version tempfiles
 	rm -f .timestamp
