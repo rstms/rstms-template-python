fb8402fe20a84ad974f1d4965534b91718b163da
Matt Krueger <mkrueger@rstms.net>
2021-11-17 16:17:04 -0600

    apply lint formatting

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -4,6 +4,7 @@
 import argparse
 {%- endif %}
 import sys
+
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 import click
 {%- endif %}
@@ -12,8 +13,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for {{cookiecutter.project_slug}}."""
-    click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
+    click.echo("Replace this message by putting your code into {{cookiecutter.project_slug}}.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 {%- endif %}
@@ -25,8 +25,7 @@ def main():
     args = parser.parse_args()
 
     print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "{{cookiecutter.project_slug}}.cli.main")
+    print("Replace this message by putting your code into {{cookiecutter.project_slug}}.cli.main")
     return 0
 {%- endif %}
 
