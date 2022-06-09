d0b6d6d625ed01d90d0f029f971eb6f688bb6df4
Matt Krueger <mkrueger@rstms.net>
2022-06-09 00:08:52 -0500

    pick a few nits

--- {{cookiecutter.project_slug}}/make.include/lint.mk
+++ {{cookiecutter.project_slug}}/make.include/lint.mk
@@ -6,8 +6,9 @@ _fmt:
 	isort $(lint_src)
 	black $(lint_src)
 
-_lint: fmt
+_lint:
 	flake8 --config tox.ini $(lint_src)
+
 
 # format source with black, check style, lint with flake8
 fmt: _fmt _lint
--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -40,16 +40,17 @@ def test_version():
     assert isinstance(__version__, str)
 
 
-def test_command_line_interface():
+def test_cli_():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli)
     assert result.exit_code != 0, result
-    assert "{{ cookiecutter.project_slug }}" in result.output
+    assert result.exception, result
+    assert "{{ cookiecutter.project_slug }}" in str(result.exception), result
 
     result = runner.invoke(cli, ["--help"])
     assert result.exit_code == 0, result
-    assert "Show this message and exit." in result.output
+    assert "Show this message and exit." in result.output, result
 {%- endif %}
 {%- else %}
 
@@ -71,8 +72,9 @@ class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli)
-        assert result.exit_code == 0, result
-        assert "{{ cookiecutter.project_slug }}.cli" in result.output, result
+        assert result.exit_code != 0, result
+        assert result.exception, result
+        assert "{{ cookiecutter.project_slug }}.cli" in str(result.exception), result
         help_result = runner.invoke(cli, ["--help"])
         assert help_result.exit_code == 0, result
         assert "--help  Show this message and exit." in help_result.output, result
