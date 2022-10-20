ea19a2600295e5dc762b52c8aecf88cf8b194c1d
Matt Krueger <mkrueger@rstms.net>
2022-10-20 02:59:50 -0500

    add default shell completion

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -11,6 +11,7 @@ import click
 
 from .version import __version__, __timestamp__
 from .exception_handler import ExceptionHandler
+from .shell import _shell_completion
 
 header = f"{__name__.split('.')[0]} v{__version__} {__timestamp__}"
 
@@ -18,13 +19,18 @@ header = f"{__name__.split('.')[0]} v{__version__} {__timestamp__}"
 @click.command("{{cookiecutter.project_slug}}")
 @click.version_option(message=header)
 @click.option("-d", "--debug", is_flag=True, help="debug mode")
+@click.option("--shell-completion", is_flag=True, help="configure shell completion")
 @click.pass_context
-def cli(ctx, debug):
+def cli(ctx, debug, shell_completion):
 
     ctx.obj = dict(ehandler = ExceptionHandler(debug))
 
+    if shell_completion:
+        return _shell_completion()
+
     """cli for {{cookiecutter.project_slug}}."""
     raise RuntimeError("Add application code to {{cookiecutter.project_slug}}/cli.py")
+
     return 0
 {%- endif %}
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/shell.py
@@ -0,0 +1,30 @@
+# shell completion
+
+from os import environ, system
+
+import click
+
+
+def _shell_completion(shell):
+    """output shell completion code"""
+    if shell == "[auto]":
+        if "ZSH_VERSION" in environ:
+            shell = "zsh"
+        else:
+            shell = "bash"
+    if shell not in ["bash", "zsh"]:
+        raise RuntimeError("cannot determine shell")
+
+    cli="{{cookiecutter.project_cli}}"
+
+    if shell == "bash":
+        click.echo(f"Writing file ~/.{cli}-complete.bash...")
+        system(f"_{cli.upper()_COMPLETE=bash_source {cli} >~/.{cli}-complete.bash")
+        click.echo("Source this file from ~/.bashrc")
+        click.echo(f"ex: . ~/.{cli}-complete.bash")
+
+    elif shell == "zsh":
+        click.echo(f"Writing file ~/.{cli}-complete.zsh...")
+        system(f"_{cli.upper()}_COMPLETE=zsh_source {cli} >~/.{cli}-complete.zsh")
+        click.echo("Source this file from ~/.zshrc")
+        click.echo(f"ex: . ~/.{cli}-complete.zsh")
