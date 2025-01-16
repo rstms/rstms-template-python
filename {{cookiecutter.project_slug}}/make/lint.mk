49cccb86f68bf3d541dffcaf4a5b510959ec8f09
Matt Krueger <mkrueger@rstms.net>
2025-01-15 19:12:04 -0700

    update lint.mk with FLAKE8_IGNORE read python version for lint

--- {{cookiecutter.project_slug}}/make/lint.mk
+++ {{cookiecutter.project_slug}}/make/lint.mk
@@ -1,11 +1,13 @@
 # python lint makefile
 
 lint_line_length := 120
-lint_python_version := 310
-
-ISORT_OPTS = --py $(lint_python_version) --profile black --gitignore
+lint_python_version != python --version | sed 's/Python \([0-9]*\)\.\([0-9]*\).*/\1\2/'
+max_complexity := 12
+ignore_errors := $(if $(FLAKE8_IGNORE),--extend-ignore $(FLAKE8_IGNORE),)
+ 
+ISORT_OPTS = --py $(lint_python_version) --profile black
 BLACK_OPTS = --target-version py$(lint_python_version) --line-length $(lint_line_length)
-FLAKE8_OPTS = --max-line-length $(lint_line_length)
+FLAKE8_OPTS = --max-line-length $(lint_line_length) --max-complexity $(max_complexity) $(ignore_errors)
 
 export ISORT_OPTS
 export BLACK_OPTS
